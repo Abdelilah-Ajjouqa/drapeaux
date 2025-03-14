dc45a29c9085e92b5e009f8d993dd2fa95b95039
Abdelilah <abdouajjouqa@gmail.com>
2025-03-14 12:43:53 +0000

    add country's methods, urls, and logic's controller

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -3,9 +3,68 @@
 namespace App\Http\Controllers;
 
 use App\Models\Country;
+use Exception;
 use Illuminate\Http\Request;
 
 class CountryController extends Controller
 {
-    
+    public function index(){
+        try{
+            $countries = Country::all();
+            return response()->json($countries, 200);
+        } catch(Exception $e) {
+            return response()->json(['message'=>"error", "error"=>$e->getMessage()], 401);
+        }
+    }
+
+    public function show($id){
+        $country = Country::find($id)->first()->get();
+
+        if(empty($country)){
+            return response()->json(['message'=>"there's no Country !"], 401);
+        }
+
+        return response()->json($country);
+    }
+
+    public function store(Request $request){
+        $validate = $request->validate([
+            'name' => 'required|string|max:255',
+            'region' => 'required|string|max:255',
+            'flag_url' => 'nullable|url',
+            'currency' => 'nullable|string|max:255',
+            'language' => 'nullable|string|max:255',
+            'capital' => 'required|string|max:255',
+        ]);
+
+        $country = Country::create($validate);
+        return response()->json($country);
+    }
+
+    public function update(Request $request, $id){
+        $country = Country::findOrFail($id);
+        $country->update($request->all());
+
+        return response()->json($country);
+    }
+
+    public function destroy($id){
+        $country = Country::findOrFail($id);
+        $country->delete();
+
+        return response()->json(null, 204);
+    }
+
+    // flags 
+    public function getFlag($id){
+        $flag = Country::findOrFail($id);
+        return response()->json($flag->flag_url, 200);
+    }
+
+    public function uploadFlag(Request $request, $id){
+        $country = Country::findOrFail($id);
+        $country->flag_url = $request->name;
+        $country->save();
+        return response()->json($country, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,17 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register'])->name("register");
 Route::post('/login', [AuthController::class, 'login'])->name("login");
-Route::post('/logout', [AuthController::class, 'logout'])->middleware("auth:sanctum");
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 
-Route::middleware('auth:sunctum')->group(function(){
+Route::middleware('auth:sanctum')->group(function(){
     Route::get('/countries', [CountryController::class, 'index'])->name("index");
+    Route::post('/countries', [CountryController::class, 'store'])->name("store");
     Route::get('/countries/{id}', [CountryController::class, 'show'])->name("show");
+    Route::post('/countries/{id}', [CountryController::class, 'update'])->name("update");
+    Route::post('/countries/{id}', [CountryController::class, 'destroy'])->name("destroy");
+
+    // flags 
+    Route::post('/countries/{id}/flag', [CountryController::class, 'uploadFlag']);
+    Route::get('/countries/{id}/flag', [CountryController::class, 'getFlag']);
 });
