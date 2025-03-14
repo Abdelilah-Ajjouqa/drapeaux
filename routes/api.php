c79bdec2c807901516df1af7a108bafff987fd47
Abdelilah <abdouajjouqa@gmail.com>
2025-03-14 11:11:32 +0000

    add Country's controller, model, and migrations needed

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CountryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,11 +10,12 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-
-// Route::get('/users', function (){
-//     return 'hola';
-// });
-
 Route::post('/register', [AuthController::class, 'register'])->name("register");
 Route::post('/login', [AuthController::class, 'login'])->name("login");
 Route::post('/logout', [AuthController::class, 'logout'])->middleware("auth:sanctum");
+
+
+Route::middleware('auth:sunctum')->group(function(){
+    Route::get('/countries', [CountryController::class, 'index'])->name("index");
+    Route::get('/countries/{id}', [CountryController::class, 'show'])->name("show");
+});
